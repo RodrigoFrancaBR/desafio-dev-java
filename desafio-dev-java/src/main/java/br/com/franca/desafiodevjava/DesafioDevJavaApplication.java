438ee66bd372f955a08c6400e52a6095bcc6b6ba
RodrigoFrancaBR <rfrancacosta@gmail.com>
2021-04-02 15:00:30 -0300

    first commit

---
+++ desafio-dev-java/src/main/java/br/com/franca/desafiodevjava/DesafioDevJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.franca.desafiodevjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDevJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDevJavaApplication.class, args);
+	}
+
+}
---
+++ desafio-dev-java/src/main/java/br/com/franca/desafiodevjava/HelloWorld.java
@@ -0,0 +1,9 @@
+package br.com.franca.desafiodevjava;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+	}
+
+}
