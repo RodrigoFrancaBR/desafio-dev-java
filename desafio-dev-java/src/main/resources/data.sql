096fc80e5da82cb6601339a5088034fcd0cc377b
RodrigoFrancaBR <rfrancacosta@gmail.com>
2021-04-05 03:41:06 -0300

    alterando para o mysql

--- desafio-dev-java/src/main/resources/data.sql
+++ desafio-dev-java/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO TB_USER (ID, NAME, GENRE, EMAIL, BIRTH_DATE, NATURALNESS, NATIONALITY, CPF, PASSWORD, USER_NAME)
+INSERT INTO tb_user (id, name, genre, email, birth_date, naturalness, nationality, cpf, password, user_name)
 VALUES 
 (1, 'Isaac Otávio Castro', 'MASCULINO', 'isaacotaviocastro@vanguarda.tv', '1986-03-18', 'Rio de Janeiro', 'Brasileira', '27217637767', '$2a$10$d89uYuqozYb6ej.RmCUJP.k5WGuMufooyzRYvYW2agOJwEn1i9E3u', 'isac'),
 (2, 'Yago Caio Assis', 'MASCULINO', 'yagoassis@email.com', '1986-06-05', 'Rio de Janeiro', 'Brasileira', '79560711784', '$2a$10$d89uYuqozYb6ej.RmCUJP.k5WGuMufooyzRYvYW2agOJwEn1i9E3u', 'yago'),
