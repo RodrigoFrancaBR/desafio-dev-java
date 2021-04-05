577f3fcb659185ee5658e58e6bf78dc549861503
RodrigoFrancaBR <rfrancacosta@gmail.com>
2021-04-04 21:24:50 -0300

    habilitando o spring security e permitindo acesso aos gets e bloqueando os demais verbos

---
+++ desafio-dev-java/src/main/java/br/com/franca/config/security/SecurityConfigurations.java
@@ -0,0 +1,29 @@
+package br.com.franca.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+	@Override
+	@Bean
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/users").permitAll();
+		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/users/*").permitAll()
+		.anyRequest().authenticated().and().csrf().disable().sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+}
